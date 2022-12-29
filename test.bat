9db3bca0ae1e3e271fb317a7b5d5666562f02b09
chris <p40000x@us.royalahold.net>
2022-12-29 14:42:58 -0500

    testing task contol options 4

--- test.bat
+++ test.bat
@@ -1,10 +1,5 @@
-@echo off
-echo Hello World from %AGENT_NAME%.
-echo My ID is %AGENT_ID%.
-echo AGENT_WORKFOLDER contents:
-@dir %AGENT_WORKFOLDER%
-echo AGENT_BUILDDIRECTORY contents:
-@dir %AGENT_BUILDDIRECTORY%
-echo BUILD_SOURCESDIRECTORY contents:
-@dir %BUILD_SOURCESDIRECTORY%
+echo Hello World from $AGENT_NAME
+echo My ID is $AGENT_ID
+echo $AGENT_WORKFOLDER contents:
+
 echo Over and out.
