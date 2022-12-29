a4e128a5689374fa9d7a2222c7bf164a61ec5789
chris <p40000x@us.royalahold.net>
2022-12-29 17:24:05 -0500

    testing task contol options 19

--- testFile.sh
+++ testFile.sh
@@ -7,4 +7,5 @@ echo "AGENT_BUILDDIRECTORY is $AGENT_BUILDDIRECTORY"
 echo "AGENT_BUILDDIRECTORY contents:"
 ls -1 $AGENT_BUILDDIRECTORY
 echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "SourcesDirectory is $(Build.SourcesDirectory)"
 echo "Over and out."
